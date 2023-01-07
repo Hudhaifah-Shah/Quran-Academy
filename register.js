6253b7a1fbdf81c24245e99813c492224fd6eb5d
Hudhaifah <hudhaifah1122@hotmail.com>
2023-01-07 11:24:22 +0500

    ;-;

--- register.js
+++ register.js
@@ -4,19 +4,3 @@ form.forEach((item, i) => {
         item.style.opacity = 1
     }, i*100)
 })
-
-const from = document.getElementById("email").value
-
-function sendEmail() {
-    Email.send({
-        Host : "smtp.gmail.com",
-        Username : "hudhaifahshah365@gmail.com",
-        Password : "password",
-        To : 'info.alkhaliqacademy@gmail.com',
-        From : from,
-        Subject : "New Student Admission",
-        Body : "And this is the body"
-    }).then(
-      message => alert(message)
-    )
-}
